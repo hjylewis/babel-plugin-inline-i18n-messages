9a460a37992015d954ab44d66aed44f8a5b3e764
hjylewis <henry.jy.lewis@gmail.com>
2020-04-25 19:30:55 -0500

    typo in examples

--- examples/react-intl-advanced/src/i18n/messages.js
+++ examples/react-intl-advanced/src/i18n/messages.js
@@ -4,7 +4,7 @@ export function addMessages(idAndMessages) {
   idAndMessages.forEach(([id, payload]) => {
     const { message, ast } = payload;
     if (messages[id] && messages[id] !== message) {
-      throw new Error('message what that id already exists');
+      throw new Error('An i18n message with that id already exists');
     }
     messages[id] = ast || message;
   });
